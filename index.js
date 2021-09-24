44298cafcf5b86b31f8e21119a33d1f68083bb5e
Đinh Sỹ Duyệt kênh <duyetsydinh@gmail.com>
2021-09-24 23:27:32 +0700

    fix all

--- index.js
+++ index.js
@@ -21,34 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-app.get("/download", cors(corsOptions), async (req, res, next) => {
-  var url = req.query.url;
-  console.log("url", url);
-  if (!ytdl.validateURL(url)) {
-    return res.sendStatus(400);
-  } else {
-    const title = "video";
-
-    await ytdl.getBasicInfo(
-      url,
-      {
-        format: "mp4",
-      },
-      (err, info) => {
-        title = info.player_response.videoDetails.title.replace(
-          /[^\x00-\x7F]/g,
-          ""
-        );
-      }
-    );
-
-    res.header("Content-Disposition", `attachment; filename="${title}.mp4"`);
-
-    ytdl(url, {
-      format: "mp4",
-    }).pipe(res);
-  }
-});
+app.get("/download", cors(corsOptions), Router);
 
 // 404
 
