7a16cc18e9f6b8ee8416be3852f1f57f1f6224db
Đinh Sỹ Duyệt kênh <duyetsydinh@gmail.com>
2021-09-25 09:19:49 +0700

    fix new src param

--- Controller/index.js
+++ Controller/index.js
@@ -2,53 +2,56 @@ const ytdl = require("ytdl-core");
 const fs = require("fs");
 const path = require("path");
 const axios = require("axios").default;
-const download = async (fileUrl, downloadFolder) => {
-  const fileName = path.basename(fileUrl);
+const download = async (req, res) => {
+  // const fileName = path.basename(fileUrl);
 
   // The path of the downloaded file on our machine
-  const localFilePath = path.resolve(__dirname, downloadFolder, fileName);
-  try {
-    const response = await axios({
-      method: "GET",
-      url: fileUrl,
-      responseType: "stream",
-    });
+  // const localFilePath = path.resolve(__dirname, downloadFolder, fileName);
+  // try {
+  //   const response = await axios({
+  //     method: "GET",
+  //     url: fileUrl,
+  //     responseType: "stream",
+  //   });
 
-    const w = response.data.pipe(fs.createWriteStream(localFilePath));
-    w.on("finish", () => {
-      console.log("Successfully downloaded file!");
-    });
-  } catch (err) {
-    throw new Error(err);
-  }
-  const VIDEO_URL = req.query.url;
-  downloadFile(VIDEO_URL, "download");
-
-  // var url = req.query.url;
-
-  // if (!ytdl.validateURL(url)) {
-  //   return res.sendStatus(400);
-  // } else {
-  //   const title = "video";
-
-  //   await ytdl.getBasicInfo(
-  //     url,
-  //     {
-  //       format: "mp4",
-  //     },
-  //     (err, info) => {
-  //       title = info.player_response.videoDetails.title.replace(
-  //         /[^\x00-\x7F]/g,
-  //         ""
-  //       );
-  //     }
-  //   );
-
-  //   res.header("Content-Disposition", `attachment; filename="${title}.mp4"`);
-  //   ytdl(url, {
-  //     format: "mp4",
-  //   }).pipe(res);
+  //   const w = response.data.pipe(fs.createWriteStream(localFilePath));
+  //   w.on("finish", () => {
+  //     console.log("Successfully downloaded file!");
+  //   });
+  // } catch (err) {
+  //   throw new Error(err);
   // }
+  // const VIDEO_URL = req.query.url;
+  // downloadFile(VIDEO_URL, "download");
+  try {
+    var url = req.query.url;
+
+    if (!ytdl.validateURL(url)) {
+      return res.sendStatus(400);
+    } else {
+      const title = "video";
+
+      await ytdl.getBasicInfo(
+        url,
+        {
+          format: "mp4",
+        },
+        (err, info) => {
+          title = info.player_response.videoDetails.title.replace(
+            /[^\x00-\x7F]/g,
+            ""
+          );
+        }
+      );
+
+      res.header("Content-Disposition", `attachment; filename="${title}.mp4"`);
+      ytdl(url, {
+        format: "mp4",
+      }).pipe(res);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
 module.exports = {
   download,
--- Route/index.js
+++ Route/index.js
@@ -3,4 +3,6 @@ const UserDownload = require("../Controller/index");
 const router = require("express-promise-router")();
 router.get("/", UserDownload.download);
 
+router.get("/:url", UserDownload.download);
+
 module.exports = router;
