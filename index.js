5eaac4c35966372fb01201f0c052772377b82582
Đinh Sỹ Duyệt kênh <duyetsydinh@gmail.com>
2021-09-25 09:13:58 +0700

    fix new route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 const cors = require("cors");
 const logger = require("morgan");
 const Router = require("./Route/index.js");
@@ -10,7 +10,7 @@ var corsOptions = {
   origin: "https://downloadvideo.vercel.app",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
-
+// cors(corsOptions),
 app.use(cors());
 //middleware
 app.use(logger("dev"));
@@ -18,40 +18,12 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.use(express.static("public"));
+
+app.get("/", cors(corsOptions), (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
-app.get("/download", cors(corsOptions), async (req, res) => {
-  try {
-    var url = req.query.url;
-
-    if (!ytdl.validateURL(url)) {
-      return res.sendStatus(400);
-    }
-    const title = "video";
-
-    await ytdl.getBasicInfo(
-      url,
-      {
-        format: "mp4",
-      },
-      (err, info) => {
-        title = info.player_response.videoDetails.title.replace(
-          /[^\x00-\x7F]/g,
-          ""
-        );
-      }
-    );
-
-    res.header("Content-Disposition", `attachment; filename="${title}.mp4"`);
-
-    return ytdl(url, {
-      format: "mp4",
-    }).pipe(res);
-  } catch (error) {
-    console.log(error);
-  }
-});
+app.use("/download", cors(corsOptions), Router);
 
 // 404
 
